6a1696d690fc21e50f954bdbd901f5bd431bbede
Brenda Nakasone <b.nakasone@ufl.edu>
2025-04-23 17:01:36 -0400

    updated tracing.js

--- tracing.js
+++ tracing.js
@@ -1,41 +1,28 @@
-'use strict';
-
-const { resourceFromAttributes } = require('@opentelemetry/resources');
-const { ATTR_SERVICE_NAME } = require('@opentelemetry/semantic-conventions');
-
-const { NodeSDK } = require('@opentelemetry/sdk-node');
-const { getNodeAutoInstrumentations } = require('@opentelemetry/auto-instrumentations-node');
-
-const { OTLPTraceExporter } = require('@opentelemetry/exporter-trace-otlp-http');
-
+/*instrumentation.js*/
+const opentelemetry = require('@opentelemetry/sdk-node');
 const {
-  ConsoleMetricExporter,
-  PeriodicExportingMetricReader,
-} = require('@opentelemetry/sdk-metrics');
+  getNodeAutoInstrumentations,
+} = require('@opentelemetry/auto-instrumentations-node');
+const {
+  OTLPTraceExporter,
+} = require('@opentelemetry/exporter-trace-otlp-proto');
+const {
+  OTLPMetricExporter,
+} = require('@opentelemetry/exporter-metrics-otlp-proto');
+const { PeriodicExportingMetricReader } = require('@opentelemetry/sdk-metrics');
 
-const { ExpressInstrumentation } = require("opentelemetry-instrumentation-express");
-const { MongoDBInstrumentation } = require("@opentelemetry/instrumentation-mongodb");
-const { HttpInstrumentation } = require("@opentelemetry/instrumentation-http");
-
-const traceExporter = new OTLPTraceExporter({
-  url: 'http://localhost:4318/v1/traces',
-});
-
-const sdk = new NodeSDK({
-  resource: resourceFromAttributes({
-    [ATTR_SERVICE_NAME]: "todo-service"
+const sdk = new opentelemetry.NodeSDK({
+  traceExporter: new OTLPTraceExporter({
+    url: 'http://localhost:4318/v1/traces',
+    headers: {},
   }),
-  traceExporter,
   metricReader: new PeriodicExportingMetricReader({
-    exporter: new ConsoleMetricExporter(),
+    exporter: new OTLPMetricExporter({
+      url: 'http://localhost:4318/v1/metrics', // url is optional and can be omitted - default is http://localhost:4318/v1/metrics
+      headers: {}, // an optional object containing custom headers to be sent with each request
+      concurrencyLimit: 1, // an optional limit on pending requests
+    }),
   }),
-  instrumentations: [
-    getNodeAutoInstrumentations(),
-    new ExpressInstrumentation(),
-    new MongoDBInstrumentation(),
-    new HttpInstrumentation(),
-  ],
+  instrumentations: [getNodeAutoInstrumentations()],
 });
-
 sdk.start();
-console.log("Tracing initialized with OTLP exporter.");
